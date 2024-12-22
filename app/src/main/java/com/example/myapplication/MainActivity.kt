51f713ac055101b7626fadbcd8081c97d20b1f92
mozid <mozid9828@gmail.com>
2024-12-22 05:11:30 -0500

    simple counter app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,11 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnINcrease.setOnClickListener {
+            val text = binding.textView.text.toString().toInt() + 1
+            binding.textView.text = "$text"
+        }
+
+
+        binding.btnDecrease.setOnClickListener {
+            val text = binding.textView.text.toString().toInt() - 1
+            binding.textView.text = "$text"
+        }
+
+        binding.btnReset.setOnClickListener {
+            binding.textView.text = "0"
+        }
+
 
 
     }
